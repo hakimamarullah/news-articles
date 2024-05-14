f275fecb134e6608713803fdd27b2d6c8e3b6b36
hakimamarullah <hakimamarullah@gmail.com>
2024-05-15 03:48:15 +0700

    Allow all origin

--- src/main/java/id/com/sonarplatform/programmingtest1/controller/ArticleDetailsController.java
+++ src/main/java/id/com/sonarplatform/programmingtest1/controller/ArticleDetailsController.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/news")
+@CrossOrigin("*")
 public class ArticleDetailsController {
 
     private final ArticleDetailsService articleDetailsService;
